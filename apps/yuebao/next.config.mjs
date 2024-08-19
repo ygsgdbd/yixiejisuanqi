30dcad82e2ca4c2c5f9a167c41d5720932c9f95b
ygsgdbd <ygsgdbd@163.com>
2024-08-19 17:18:29 +0800

    feat(项目基础配置):

--- apps/yuebao/next.config.mjs
+++ apps/yuebao/next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
-    images: {
-        unoptimized: true
-    }
+  reactStrictMode: true,
+  transpilePackages: ["@repo/ui"],
 };
 
 export default nextConfig;
---
+++ apps/yuebao/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-red-500">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea impedit ipsa ipsam iste necessitatibus numquam obcaecati porro quod rem vel. Dolore doloremque minima nulla perspiciatis rem similique tempora, ullam vel?
+    </main>
+  );
+}
